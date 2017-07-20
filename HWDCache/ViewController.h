d35dcbdd45caeb7228d840a8457289bf75f6f540
hwd <huwd@adnonstop.com>
2017-07-20 17:08:19 +0800

    Initial Commit

---
+++ HWDCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWDCache
+//
+//  Created by echo_hu on 20/7/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HWDCache/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HWDCache
+//
+//  Created by echo_hu on 20/7/17.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
